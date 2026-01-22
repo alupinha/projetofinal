2c0c8db633d95cdcecd598a0844712da0c0efcd6
Paula <paularoessle@gmail.com>
2026-01-22 23:05:22 +0000

    Inseri conteudo nos componentes

--- src/app/componentes/main/main.ts
+++ src/app/componentes/main/main.ts
@@ -1,11 +1,4 @@
-import { Component } from '@angular/core';
 
-@Component({
-  selector: 'app-main',
-  imports: [],
-  templateUrl: './main.html',
-  styleUrl: './main.css',
-})
-export class Main {
-
-}
+<main >
+  <router-outlet />
+</main>
